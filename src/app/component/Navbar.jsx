8773986a6f8bf83fa9a7ac482e121455edf1c8ca
Satyam Gupta <guptasatyam1709@gmail.com>
2024-06-10 21:28:22 +0530

    frontend start

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <>
+        <div className = "bg-blue-500">
+            <h1 className = " text-white">Dextra Rehab</h1>
+        </div>
+        </>
+    )
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navbar/>
       <body className={inter.className}>{children}</body>
+      <Footer/>
     </html>
   );
 }
