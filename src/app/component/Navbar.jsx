a6f72d55c17d334535c4518e5a3c8b92c63720a9
agarimaa <Agarimaa@users.noreply.github.com>
2024-06-11 10:35:10 +0530

    navbar+footer

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -1,9 +1,12 @@
 export default function Navbar(){
     return (
-        <>
-        <div className = "bg-blue-500">
-            <h1 className = " text-white">Dextra Rehab</h1>
+        <div className="w-full max-w-[1620px] h-[73px] bg-[#163172] flex items-center justify-between px-4 py-2">
+        <div className="flex items-center">
+            {/* Logo */}
+            <img src="Dextra-Rehab-Councelling-Centre\logo_img.png" alt="Dextra Logo" className="h-8 w-auto mr-2" />
+            {/* Title */}
+            <h1 className="text-white">Dextra Rehab</h1>
         </div>
-        </>
+    </div>
     )
 }
